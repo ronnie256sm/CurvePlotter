1570a8e63c8d7c235cf556a34750be1a195c7402
Ronnie256 <drronnie256@gmail.com>
2025-04-15 15:24:49 +0700

    убрал лишнее

--- splines_avalonia/SmoothingSpline.cs
+++ splines_avalonia/SmoothingSpline.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Runtime.CompilerServices;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
